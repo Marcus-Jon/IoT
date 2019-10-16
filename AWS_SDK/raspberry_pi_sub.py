52865278df33f252b867d98b46365755ba783924
Marcus-Jon <47177527+Marcus-Jon@users.noreply.github.com>
2019-10-16 18:51:42 +0100

    Update raspberry_pi_sub.py

--- AWS_SDK/raspberry_pi_sub.py
+++ AWS_SDK/raspberry_pi_sub.py
@@ -8,24 +8,37 @@ import json
 
 def customCallback(client, userdata, message):
     print "new message received"
+    print message.topic
     print message.payload
 
 def cert_assignment():
     MQTTclient = AWSIoTMQTTClient("rasp_pi_sub")
+    Shadowclient = AWSIoTMQTTShadowClient("rasp_pi_sub_shadow")
     MQTTclient.configureEndpoint("endpoint", 8883)
+    Shadowclient.configureEndpoint("endpoint", 8883)
     MQTTclient.configureCredentials("rootCA.pem", "private key", "certificate")
-    return MQTTclient
+    Shadowclient.configureCredentials("rootCA.pem", "private key", "certificate")
+    return MQTTclient, Shadowclient
 
-def connect(client):
+def sub_MQTT(client, topic):
     client.connect()
     while True:
-        client.subscribe("test_pi", 1, customCallback)
+        client.subscribe(topic, 1, customCallback)
     client.disconnect()
+
+def sub_shadow(shadow_client):
+    shadowclient = shadow_client.getMQTTConnection()
+    shadowclient.connect()
+    while True:
+        shadowclient.subscribe("shadow update accepted topic", 1, customCallback)
+    shadowclient.disconnect()
 
 def main():
     print "MQTT subscriber"
-    client = cert_assignment()
-    connect(client)
+    topic = "test_pi"
+    client, shadow_client = cert_assignment()
+    sub_shadow(shadow_client)
+    #sub_MQTT(client, topic)
 
 if __name__ == '__main__':
     main()
